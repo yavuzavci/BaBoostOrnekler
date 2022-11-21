44e9bcb5a0a8b2a9d0066c7207d1bed5d441f6df
yavuzavci <yavuzavci@gmail.com>
2022-11-21 17:49:28 +0300

    ders sonu ödevleri çözüldü

--- src/com/bilgeadam/lesson012/Test.java
+++ src/com/bilgeadam/lesson012/Test.java
@@ -1,6 +1,5 @@
 package com.bilgeadam.lesson012;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 /*
@@ -8,25 +7,55 @@ import java.util.Scanner;
  * 2. method illeri plaka kodları ile yazdıran kod
  * 3. method girdiğimiz şehrin ismine göre plaka dönen methodu yazalım
  * 4. ilk harf hariç sesli harfleri silip yazdıran metod
+ * 5.method bir önceki methoddan dönen Dğerlerin ilk üç harfini alın ve sonuna ... 
+ * nokta ekleyin Adn... Ady... gibi
+ * 6.method bir önceki methoddan dönen dizide  B ile başlayan illerin başına 1-Blk gibi yazdıralım
+ * 7.method ikinci harfi a olanların  ilk değeri ve sadece ilk 3 harfini büyük yazdırın Çankırı->ÇAN
  */
 public class Test {
 	
 	public static void main(String[] args) {
 		SehirDatabase database = new SehirDatabase();
 		
-		String[] iller = database.iller;
+//		String[] iller = database.iller;
+		String[] iller;
 		
-		//ileBaşlayanİller(iller);
-		//illeriPlakalariylaYazdir(iller);
-		//String plaka = plakaBul(iller);
-		//if(Integer.valueOf(plaka) <= 0)
-		//	System.err.println("Girdiğiniz il için plaka bulunamadı.");
-		//else
-		//	System.out.println("Plaka..: " + plaka);
+//		ileBaşlayanİller(iller);
+//		illeriPlakalariylaYazdir(iller);
+//		String plaka = plakaBul(iller);
+//		if(Integer.valueOf(plaka) <= 0)
+//			System.err.println("Girdiğiniz il için plaka bulunamadı.");
+//		else
+//			System.out.println("Plaka..: " + plaka);
+//		
+//		ileBaşlayanİller(database.iller);
+//		harfSilerekYazdir(iller);
 		
-		//ileBaşlayanİller(database.iller);
+		/* buradan sonraki kısımlar 2022-11-21 uygulama dersi sonunda
+		 * denenmesi istenen beşinci, altıncı ve yedinci metotları içerecektir. 
+		 * Çalıştırmak istemediğiniz metodun kod satırını yorum içine alıp 
+		 * tersi durumda yorum dışı bırakın.
+		 */
 		
-		harfSilerekYazdir(iller);
+		// 5. method
+//		iller = sesliHarfleriSil(database.iller);
+//		illeriYazdir(iller);
+		// 6. method
+//		bIleBaşlayanIller(iller);		
+		// 7. method
+		iller = database.iller;
+		illeriSecerekYazdir(iller);
+	}
+	
+	public static void bIleBaşlayanIller(String[] iller) {
+		int bSayaci = 1;
+		for (int i = 0; i < iller.length; i++) {
+			if(iller[i].startsWith("B")) {
+				System.out.println(bSayaci + "-" + iller[i]);
+				bSayaci++;
+			}
+				
+		}
 	}
 	
 	public static void ileBaşlayanİller(String[] dizi) {
@@ -37,6 +66,19 @@ public class Test {
 		for (int i = 0; i < dizi.length; i++) {
 			if(dizi[i].startsWith(harf.toUpperCase().substring(0,1)))
 				System.out.println(dizi[i]);
+		}
+	}
+	
+	public static void illeriSecerekYazdir(String[] iller) {
+		for (int i = 0; i < iller.length; i++) {
+			if(iller[i].indexOf('a') == 1)
+				System.out.println(iller[i] + "->" + iller[i].substring(0,3).toUpperCase());
+		}		
+	}
+	
+	public static void illeriYazdir(String[] iller) {
+		for (int i = 0; i < iller.length; i++) {
+			System.out.println(iller[i]);
 		}
 	}
 	
@@ -69,18 +111,19 @@ public class Test {
 		return sonuc;
 	}
 	
-	public static void harfSilerekYazdir(String[] iller) {
+	public static String[] sesliHarfleriSil(String[] iller) {
 		String[] sesliHarfler = { "a", "e", "ı", "i", "o", "ö", "u", "ü" }; 
 		for (int i = 0; i < iller.length; i++) {
 			String sonuc = "";			
 				
 			for (int j = 0; j < sesliHarfler.length; j++) {
 				if(iller[i].contains(sesliHarfler[j])) {
-					sonuc = iller[i].substring(0,1) + iller[i].substring(1)
+					iller[i] = iller[i].substring(0,1) + iller[i].substring(1)
 							.replace(sesliHarfler[j],"");
 				}												
-			}
-			System.out.println(sonuc);
+			}			
 		}
+		
+		return iller;
 	}
 }
