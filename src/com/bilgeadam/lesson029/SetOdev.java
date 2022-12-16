2326ce6c8c424c96d444d71aafca7b22e26a86e9
yavuzavci <yavuzavci@gmail.com>
2022-12-16 16:58:50 +0300

    set ödevi eklendi

---
+++ src/com/bilgeadam/lesson029/SetOdev.java
@@ -0,0 +1,21 @@
+package com.bilgeadam.lesson029;
+/*
+* Dışarıdan bir string deger alacağız 
+* 
+*bu string degerdeki her harfi kontrol edeceğiz tekrar edenleri bir sete 
+*tekrar etmeyenleri başka bir sete atacagız 
+* 
+* daha sonra bu iki seti yazdıracagız
+* 
+* araba
+* 
+* tekrar edenler
+* a
+* tekrar etmeyenler
+* r
+* b
+* 
+*/
+public class SetOdev {
+
+}
