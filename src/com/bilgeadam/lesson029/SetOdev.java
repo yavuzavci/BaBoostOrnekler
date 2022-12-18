b243d71376bd0a86adf6677876a9c8b2f9b3760d
yavuzavci <yavuzavci@gmail.com>
2022-12-18 22:52:26 +0300

    set ödevi tamamlandı

--- src/com/bilgeadam/lesson029/SetOdev.java
+++ src/com/bilgeadam/lesson029/SetOdev.java
@@ -1,4 +1,9 @@
 package com.bilgeadam.lesson029;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /*
 * Dışarıdan bir string deger alacağız 
 * 
@@ -17,5 +22,73 @@ package com.bilgeadam.lesson029;
 * 
 */
 public class SetOdev {
-
+	
+	public static final Set<Character> tekrarEdenHarfler = new HashSet<Character>();
+	public static final Set<Character> tekrarEtmeyenHarfler = new HashSet<Character>();
+	public static final Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		uygulama();
+	}
+	
+	public static void uygulama() {
+		yazdir(degerAl());
+	}
+	
+	public static String degerAl() {
+		System.out.println("Lütfen bir ifade giriniz");
+		String ifade = scanner.nextLine();
+		scanner.close();
+		return ifade;
+	}
+	
+	public static void yazdir(String ifade) {
+		if(ifade.isEmpty())
+			System.err.println("HATA: Boş ifadede tekrar eden ifade olmaz.");
+		else {			
+			if(ifade.length() == 1)
+				System.out.println("İfadeniz " + ifade + " tek harf içerir.");
+			else {
+				harfleriBul(ifade);
+				tekrarlariSil(tekrarEtmeyenHarfler);
+				tekrarEdenleriYazdir(tekrarEdenHarfler);
+				tekrarEtmeyenleriYazdir(tekrarEtmeyenHarfler);
+			}
+		}		
+	}
+	
+	public static void harfleriBul(String ifade) {
+		char[] harfler = ifade.toCharArray();
+		for(char harf : harfler) {
+			if(tekrarEtmeyenHarfler.add(harf))
+				continue;
+			tekrarEdenHarfler.add(harf);
+		}		
+	}
+	
+	public static void tekrarlariSil(Set<Character> tekrarEtmeyenHarfler) {
+		for(Character harf : tekrarEdenHarfler) {
+			tekrarEtmeyenHarfler.remove(harf);
+		}		
+	}
+	
+	public static void tekrarEdenleriYazdir(Set<Character> tekrarEdenHarfler) {
+		if(tekrarEdenHarfler.isEmpty())
+			System.out.println("Tekrar eden harf yoktur.");
+		else {
+			System.out.println("########## Tekrar Eden Harfler ##########");
+			tekrarEdenHarfler.forEach(System.out::println);
+		}		
+	}
+	
+	public static void tekrarEtmeyenleriYazdir(Set<Character> tekrarEtmeyenHarfler) {
+		if(tekrarEtmeyenHarfler.isEmpty())
+			System.out.println("Tekrar etmeyen harf yoktur.");
+		else {
+			System.out.println("######### Tekrar Etmeyen Harfler #########");
+			tekrarEtmeyenHarfler.forEach(System.out::println);
+		}
+		
+	}
+	
 }
