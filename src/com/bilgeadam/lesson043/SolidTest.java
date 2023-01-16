cfe18a27d4906d75f1f0e29a50957be24b5502cf
yavuzavci <yavuzavci@gmail.com>
2023-01-16 16:49:36 +0300

    solid prensiplerine giriş yapıldı

---
+++ src/com/bilgeadam/lesson043/SolidTest.java
@@ -0,0 +1,12 @@
+package com.bilgeadam.lesson043;
+/*
+ * S => Single Responsibility ==> Bir nesne veya bir metodun tek bir görevi olması lazım
+ * O => Open - Closed ==> Kodlarımız geliştirmeye açık ama değiştirmeye kapalı olmalıdır.
+ * L => Liskov Substitution ==> Bir sınıf miras aldığı sınıf gibi davranmalı
+ * I => Interface Segregation ==> Sorumlulukların hepsi tek arayüze yüklenmemeli
+ * D => Dependency Inversion ==> Sınıflar arası bağlılıkları en alt düzeyde tutmak lazım
+ * özellikle üst seviyedeki bir sınıf alt seviyedeki bir sınıfa bağlı kalmamalıdır.
+ */
+public class SolidTest {
+
+}
