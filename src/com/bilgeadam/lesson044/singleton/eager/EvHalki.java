6922d8c282a3ea5c8d7315968826e44f2a8ad0d5
yavuzavci <yavuzavci@gmail.com>
2023-01-17 16:09:46 +0300

    eager singleton oluşturma yöntemi eklendi

---
+++ src/com/bilgeadam/lesson044/singleton/eager/EvHalki.java
@@ -0,0 +1,20 @@
+package com.bilgeadam.lesson044.singleton.eager;
+
+public class EvHalki implements Runnable {
+	private Robot robot;
+	
+	public void robotCagir() {
+		robot = Robot.getInstance();
+		robot.calis();
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		robotCagir();
+	}
+}
